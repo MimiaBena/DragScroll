bec84baf88a6dd38cc7f7d974dbe58324a4e1081
Benaouali Mokhtaria <benaoualimokhtaria@hotmail.com>
2021-12-15 17:32:12 +0100

    update code

--- appJs.js
+++ appJs.js
@@ -1,17 +1,39 @@
 const contentSection = document.querySelector(".content");
 
-function test(){
-    for(let i = 0; i < 9; ++i) {
+
+function createItem(){
+    for(let i = 0; i < 7; ++i) {
     const item = document.createElement('div');
     item.classList.add('post');
-    item.style.backgroundColor = 'blue';
-    
-    
-    contentSection.append(item);
-
-    
+    item.style.backgroundColor = 'blue';  
+    contentSection.append(item);   
 }
-
-
 }
-test();
+createItem();
+
+let isDown = false;
+let startX;
+let scrollLeft;
+contentSection.addEventListener('mousedown', (e) =>
+ {
+    isDown= true;
+    contentSection.style.cursor = 'grabbing';
+    startX = e.pageX - contentSection.offsetLeft;
+    scrollLeft = contentSection.scrollLeft;
+    
+});
+
+contentSection.addEventListener('mouseleave', () =>
+{
+    isDown= false;
+});
+
+contentSection.addEventListener('mouseup', () => {
+    isDown = false;
+  });
+  contentSection.addEventListener('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - contentSection.offsetLeft;
+    contentSection.scrollLeft = scrollLeft + ((startX - x) * 3);
+  });
